61fd37e791ea31971abcb2e1e47fdfbfe6c50f45
AhosanKabir <kaziahosan1998@gmail.com>
2025-05-23 23:40:20 +0600

    odoo 18 module structure

---
+++ __manifest__.py
@@ -0,0 +1,31 @@
+{
+    'name': 'Module Name',
+    'version': '18.0.0.1',
+    'depends': [],
+    'category': '',
+    'summary': """
+        module summary goes to here
+    """,
+    'description': """
+        module details goes to here
+    """,
+    'author': 'Author Name',
+    'co-author': 'Co-Author name',
+    'website': 'https://company_website_url.com',
+    'data': [
+        # 'security/ir.model.access.csv',
+        # "views/templates.xml"
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            # '/module_name/static/src/js/.js',
+            # '/module_name/static/src/xml/.xml',
+            # '/module_name/static/src/scss/.scss',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'sequence': 1,
+    'license': 'LGPL-3',
+}
